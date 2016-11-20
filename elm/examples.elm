8fd3b364e8efa334852ebb4db7c95fc33665ef8d
Jell <jean-louis@jawaninja.com>
2016-11-20 16:20:39 +0100

    First pass of frontend intro

---
+++ elm/examples.elm
@@ -0,0 +1,53 @@
+module Examples
+import String
+
+if True then "hello" else "goodbye"
+
+inc = \x -> x + 1
+{- or -}
+inc2 x = x + 1
+{- or -}
+inc3 = (+) 1
+
+fib n = case n of
+            0 -> 1
+            1 -> 1
+            _ -> fib (n-1) + fib (n-2)
+
+
+map fn xs = case xs of
+                [] -> []
+                head :: tail -> (fn head) :: map fn tail
+
+mapped = map inc [1, 2, 3]
+
+reduce fn accu xs =
+    case xs of
+        [] -> accu
+        head :: tail -> (reduce fn (fn accu head) tail)
+
+reduced = reduce (+) 0 [1, 2, 3, 4]
+
+filter predicate xs =
+    case xs of
+        [] -> []
+        head :: tail -> if (predicate head)
+                        then head :: filter predicate tail
+                        else filter predicate tail
+
+even x = (x % 2) == 0
+odd x = not (even x)
+
+filtered = filter even [1, 2, 3, 4]
+
+
+eval things = case things of
+               [] -> ""
+               ["surprise", x] -> String.concat [x, "!!!"]
+               "concat" :: rest -> String.concat rest
+               _ -> "no match"
+
+
+twice = (*) 2
+incAndDouble = inc >> twice
+doubleAndInc = inc << twice
---
+++ javascript/intro.js
@@ -0,0 +1,110 @@
+"hello";
+
+a = "hello";
+
+a;
+
+if (true) {
+  "hello";
+} else {
+  "goodbye";
+}
+
+function inc(x) { return x + 1; }
+inc = function (x) { return x + 1; };
+inc = (x) => {return x + 1;};
+inc = (x) => x + 1;
+
+inc(5);
+inc(6);
+inc;
+
+
+1 + 1;
+
+var add = (x, y) => x + y;
+
+add(4, 5);
+
+
+
+var applyOneOne = (f) => f(1,1);
+
+applyOneOne(add);
+
+
+var apply = (f, x, y) => f(x, y);
+
+apply(add, 2, 3);
+
+
+var makeIncrementer = (n) => (m) => m + n;
+
+var add6 = makeIncrementer(6);
+
+add6(4);
+
+
+var partial = (f, ...args) => (...more) => f(...args, ...more);
+
+var add1 = partial(add, 1);
+add1(2);
+
+var useless = partial(add, 1, 2);
+useless();
+
+var addbis = partial(add);
+
+addbis(1,2);
+
+
+
+[1,2,3].map(inc);
+
+var map = (fn, things) => {
+  var mapped = [];
+  things.forEach((element) => mapped.push(fn(element)));
+  return mapped;
+};
+
+map(inc, [1,2,3]);
+
+[1,2,3].map(inc);
+
+Array.prototype.map.call([1,2,3], inc);
+
+var isEven = (n) => (n % 2) == 0;
+var isOdd = (n) => !isEven(n);
+
+var filter = (predicate, things) => {
+  var filtered = [];
+  things.forEach((element) => {
+    if (predicate(element)) {
+      filtered.push(element);
+    }});
+  return filtered;
+};
+
+filter(isEven, [1,2,3,4]);
+
+var reduce = (fn, start, things) => {
+  var result = start;
+  things.forEach((element) => {
+    result = fn(result, element);
+  });
+  return result;
+};
+
+reduce(add, 0, [1, 2, 3]);
+
+var addElement = (things, element) =>
+    Array.prototype.concat.call(things, element);
+
+var mapBis = (fn, things) =>
+    reduce(
+      (result, element) => addElement(result, fn(element)),
+      [],
+      things
+    );
+
+mapBis(inc, [1,2,3]);
